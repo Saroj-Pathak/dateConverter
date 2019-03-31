adbb3ce052c30aa5a451cdcb871260e143934fb4
Saroj-Pathak <46081213+Saroj-Pathak@users.noreply.github.com>
2019-03-31 17:25:58 +0545

    Update DateConverter_app.R

--- DateConverter_app.R
+++ DateConverter_app.R
@@ -1,44 +1,69 @@
 
 library(shiny)
+library(shinyWidgets)
 
 source("./global.R")
 
+# Define UI for application
 
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel(h1("Convert BS to AD", align = "center", style = "color:Red")),
-   
-   # Sidebar with a dateInput and submitButton
-   sidebarLayout(
+    
+    # Application title
+    titlePanel(h1("Convert BS to AD", align = "center", style = "color:Red")),
+    
+    # Sidebar with a dateInput and submitButton
+    sidebarLayout(
       sidebarPanel(
-        dateInput("Input_date",
-                  "Enter Date in yyy/mm/dd format"),
+        
+        radioButtons("rdo", "Convert to :", c("BS to AD " = "toAD",
+                                         "AD to BS " = "toBS")),
+        br(),
+        dateInput("date",
+                  "Enter Date in yyy/mm/dd format"
+                  ),
+        
+        submitButton("Submit"),
+                     
+        br(),
+        br(),
+        br()
+      ),
       
-        submitButton("Submit")
-        ),
-      
-      # Show a text
+      # Show a plot of the generated distribution
       mainPanel(
         verbatimTextOutput("date")
       )
-   )
-)
+    )
+  )
 
-# Define server logic 
+  
+# Define server logic
 server <- function(input, output) {
-   
    output$date <- renderText({
-     
      #Date to be converted into AD ------
-     input.date <- input$Input_date
-     pdate <- BS_to_AD(input.date)
-     paste ("The converted date is   ", pdate %>%
-       format("%d %b %Y %A"))
+     input.date <- input$date
+     input.rd <- input$rdo
+     
+      if (input.rd == "toAD") {
+        pdate <- BS_to_AD(input.date)
+        paste ( "The converted date is  ",
+               pdate %>%
+                 format("%d %B %Y %A")
+               )
+      }
+     else {
+       pdate <- AD_to_BS(input.date)
+       paste ("The converted date is  ", 
+              pdate,
+              input.date %>%
+                format ("%A"))
+     }
    })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
 
 
